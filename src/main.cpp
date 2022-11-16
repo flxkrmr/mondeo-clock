9fd7c77a29c245c52485e0a1fe665d94d6dbf414
flxkrmr <felixkramerroki@aol.com>
2022-11-16 09:00:41 +0100

    Refactor for increment based display function

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,19 @@
 #include <Wire.h>
 #include "RTClib.h"
 
+#define REFRESH_DELAY_MS 10
+
 RTC_DS1307 rtc;
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
+
+unsigned short inc = 0;
 
 void setupSerial();
 void setupRtc();
 void setupU8g2();
 
-void displayTime(char delim);
+void displayTime(unsigned short i);
 
 void setup() {
   setupSerial();
@@ -19,11 +23,11 @@ void setup() {
   setupU8g2();
 }
 
+
 void loop() {
-  displayTime(':');
-  delay(500);
-  displayTime(' ');
-  delay(500);
+  displayTime(inc);
+  delay(REFRESH_DELAY_MS);
+  inc++;
 }
 
 
@@ -49,12 +53,21 @@ void setupU8g2() {
   u8g2.begin();
 }
 
-void displayTime(char delim) { 
+#define TIME_REFRESH_CYCLES 50 // with 10 ms delay this is each half second
+bool delim_present = true;
+void displayTime(unsigned short i) {
+  if (i % TIME_REFRESH_CYCLES) {
+    return;
+  }
+
   char buf[10];
+  char delim = delim_present ? ':' : ' ';
+  delim_present = !delim_present;
+
   DateTime now = rtc.now();
 
   sprintf(buf, "%02d%c%02d", now.hour(), delim, now.minute());
-          
+
   u8g2.setFont(u8g2_font_inr30_mf);
   u8g2.clearBuffer();
   u8g2.drawStr(0,32,buf);
